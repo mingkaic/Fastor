b551cd63927d0d87c1778dcaaa363a5b0e7df0cd
mingkaic <mingkaichen2009@gmail.com>
2024-02-26 20:43:34 -0800

    Add option to disable ASM dependent util file

--- Fastor/util/util.h
+++ Fastor/util/util.h
@@ -1,7 +1,10 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+// disable hardcode dependent
+#ifndef DISABLE_ASM_INTERACTION
 #include "Fastor/util/timeit.h"
+#endif // DISABLE_ASM_INTERACTION
 #include "Fastor/util/print.h"
 #include "Fastor/util/write.h"
 #include "Fastor/util/types.h"
